19abae3b70555a957d7f7f83d1a1b9d7c621a422
Ahmed Hamdy <dimo.hamdy@gmail.com>
2018-10-24 17:27:43 +0200

    Initial Commit

---
+++ OpenCV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenCV
+//
+//  Created by BinaryBoy on 10/24/18.
+//  Copyright © 2018 BinaryBoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
